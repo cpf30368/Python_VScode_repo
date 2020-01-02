a99df5b201f6fc60561fce8a904d4914e5d87a69
Christian Pfeiffer <cpf303@hotmail.com>
2020-01-02 17:10:12 +0100

    Inital Commit

---
+++ exercises/user_input/ex-12.py
@@ -0,0 +1,14 @@
+import sys
+#import requests
+import os
+import math
+
+print(sys.version)
+print(sys.executable)
+# age = input("How old are you? ")
+# height = input("How tall are you? ")
+# weight = input("How much do you weigh? ")
+# print(f"So, you're {age} old, {height} tall, and {weight} heavy.")
+#r = requests.get("http://www.aktienkursentstehung.de")
+#print(r.status_code)
+
